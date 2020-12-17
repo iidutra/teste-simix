3c1aca2b8109b3ef55da93a8812a1f5d2e78e982
igor dutra <igordutra07@gmail.com>
2020-12-17 12:35:20 -0400

    Add project files.

---
+++ teste-simix/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using teste_simix.Models;
+
+namespace teste_simix.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Resultado()
+        {
+            var numeros = "";
+            for (int i = 1; i <= 200; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    numeros += " Z ";
+                }
+                else if (i % 3 == 0)
+                {
+                    numeros += " X ";
+                }
+                else if (i % 5 == 0)
+                {
+                    numeros += " Y ";
+                }
+                else
+                {
+                    numeros += i.ToString()+" ";
+                }
+            }
+            ViewData["Numeros"] = numeros;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ teste-simix/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace teste_simix.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
